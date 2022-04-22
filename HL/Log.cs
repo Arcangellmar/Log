daea8607b9599375f5b0598c30e7e43fd3de3f6d
Rolando Jesus Zafra Moran <rolando20022016@hotmail.com>
2022-04-21 22:00:36 -0500

    Initial commit

---
+++ HL/Log.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HL
+{
+    public class Log
+    {
+        public static void RegistroLog(String message)
+        {
+            try
+            {
+                String getName = DateTime.Now.ToString("dd-MM-yyyy");
+                String folderTemp = AppDomain.CurrentDomain.BaseDirectory;
+                String dirLog = "Logs";
+
+                String path = Path.Combine(folderTemp, dirLog, getName + ".txt");
+
+                if (!Directory.Exists(Path.Combine(folderTemp, dirLog))) Directory.CreateDirectory(Path.Combine(folderTemp, dirLog));
+
+                if (!System.IO.File.Exists(path)) System.IO.File.Create(path).Dispose();
+
+                StreamWriter file;
+                file = System.IO.File.AppendText(path);
+                file.WriteLine(DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss"));
+                file.WriteLine(message);
+                file.WriteLine();
+                file.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
